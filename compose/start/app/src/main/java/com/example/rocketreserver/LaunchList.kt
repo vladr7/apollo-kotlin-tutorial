5245543b7c8cd5839cb6b675addc2be2218ad97d
Vlad <ricean.vlad7@gmail.com>
2023-06-27 12:12:59 +0300

    Display list of LaunchSites into LazyColumn

--- compose/start/app/src/main/java/com/example/rocketreserver/LaunchList.kt
+++ compose/start/app/src/main/java/com/example/rocketreserver/LaunchList.kt
@@ -10,12 +10,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -23,17 +28,20 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun LaunchList(onLaunchClick: (launchId: String) -> Unit) {
+    var launchList by remember { mutableStateOf(emptyList<LaunchListQuery.Launch>()) }
+
     LaunchedEffect(
         key1 = Unit,
     ) {
         val response = apolloClient.query(LaunchListQuery()).execute()
+        launchList = response.data?.launches?.launches?.filterNotNull() ?: emptyList()
         Log.d("LaunchList", "Success ${response.data}")
     }
 
     LazyColumn {
-        items(20) {
+        items(launchList) { launch ->
             LaunchItem(
-                launchId = it.toString(),
+                launch = launch,
                 onClick = onLaunchClick
             )
         }
@@ -41,12 +49,12 @@ fun LaunchList(onLaunchClick: (launchId: String) -> Unit) {
 }
 
 @Composable
-private fun LaunchItem(launchId: String, onClick: (launchId: String) -> Unit) {
+private fun LaunchItem(launch: LaunchListQuery.Launch, onClick: (launchId: String) -> Unit) {
     ListItem(
-        modifier = Modifier.clickable { onClick(launchId) },
+        modifier = Modifier.clickable { onClick(launch.id) },
         headlineText = {
             // Mission name
-            Text(text = "Launch $launchId")
+            Text(text = "Launch ${launch.id}")
         },
         supportingText = {
             // Site
